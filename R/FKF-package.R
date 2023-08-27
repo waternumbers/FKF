bfb64c8db178550d350683f65c2ac902f6781ea9
Paul Smith <paul@waternumbers.co.uk>
2023-08-27 19:25:31 +0100

    Add package documentation

--- R/FKF-package.R
+++ R/FKF-package.R
@@ -1,12 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' @useDynLib FKF
 #' @import graphics
-# #' @import RUnit
 #' @importFrom grDevices rainbow
 #' @importFrom stats acf ccf ppoints qchisq qnorm qqnorm qqplot
 #' @exportClass fkf
 NULL
-##export("fkf", "plot.fkf")
-##exportClasses("fkf")
-## export plot method for class fkf
-##S3method("plot", "fkf")
 
